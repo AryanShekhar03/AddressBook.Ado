32bb056623e483fe6c732b8574b0d7ca4a0bb5aa
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-01-30 16:21:54 +0530

    Aaryan added UC3 program

--- Program.cs
+++ Program.cs
@@ -9,10 +9,25 @@ namespace AddressBook.ADO
             Console.WriteLine("Welcome to AddressBook");
 
             AddressBookRepo addressBookRepo = new AddressBookRepo();
+
             AddressBookModel addressBookModel = new AddressBookModel();
 
-            addressBookRepo.DatabaseConnection();
-            addressBookRepo.GetAllContact();
+            addressBookModel.FirstName = "Mr";
+            addressBookModel.LastName = "Haltung";
+            addressBookModel.Address = "AAAAA";
+            addressBookModel.City = "CCCCC";
+            addressBookModel.State = "MMMMM";
+            addressBookModel.Zipcode = 44444;
+            addressBookModel.PhoneNumber = 999999999;
+            addressBookModel.Email = "mrhaltung@gmail.com";
+            addressBookModel.AddressBookName = "MyBook";
+            addressBookModel.AddressBookType = "family";
+            bool result = addressBookRepo.AddContact(addressBookModel);
+            // addressBookRepo.DatabaseConnection();
+
+            //addressBookRepo.GetAllContact();
+
+              addressBookRepo.AddDataToTable(addressBookModel);
         }
     }
 }
